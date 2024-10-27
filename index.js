c743314187bd75e20ff8e897264fc244c9a9febd
impactgalaxy <traderglow@gmail.com>
2024-10-28 02:27:33 +0600

    create and first commit

--- index.js
+++ index.js
@@ -3,40 +3,63 @@ const express = require("express");
 // const fs = require("fs");
 const path = require("path");
 const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const app = express();
-
+require("dotenv").config()
 app.use(cors());
 app.use(express.json());
 app.use(
   cors({
     origin: [
       "https://my-portfolio-f8f09.web.app",
-    ]
+      "http://localhost:5173"
+    ],
+    methods: ["PUT", "GET", "POST", "PATCH"]
+    
   })
 );
 
-//generate PDF doc 
-// const generatePDF = (filePath, content) => {
-//   const doc = new PDFDocument();
-//   doc.pipe(fs.createWriteStream(filePath));
-//   doc.text(content);
-//   doc.end();
-// };
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.s7sbkwf.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+const run = async() => {
+  try {
+    const usersCollection = client.db("MyPortfolio").collection("users");
+    const usersMessageCollection = client.db("MyPortfolio").collection("usersMessage")
+
+    app.post("/nur_mohammad_palash_portfolios_users", async (req, res) => {
+      const { uid, emailVerified, providerData } = req.body;
+      const isAlreadyExist = await usersCollection.findOne({ uid })
+      if (isAlreadyExist) return;
+      await usersCollection.insertOne({ uid, emailVerified, providerData });
+      return res.send("done");
+    })
+
+    app.post("/users_message", async (req, res) => {
+      const data = req.body;
+      const result = await usersMessageCollection.insertOne(data);
+      res.send(result)
+    })
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  }  finally {
+    
+  }
+}
+run().catch(console.dir)
 
 // get method for pdf
 app.get('/download-pdf', (req, res) => {
    
-    const filePath = path.join(__dirname, "files", 'resume.pdf');
-
-    // const filePath = path.join(__dirname,  'resume2.pdf');
-    
-    // If I want to generate new document;
-  
-    //   generatePDF(filePath, content);
-    
-    // download existing file
-    
+    const filePath = path.join(__dirname, "files", 'resume.pdf'); 
 
   res.download(filePath, 'resume_of_palash.pdf', (err) => {
     if (err) {
